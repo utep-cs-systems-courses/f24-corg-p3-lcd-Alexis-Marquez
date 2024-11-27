38441542f7bbcb4583646266cb0841c758eb842b
Alexis Marquez <alexismarq337@gmail.com>
2024-11-27 10:58:48 -0700

    fixed issue with header file

--- project/main.h
+++ project/main.h
@@ -4,15 +4,15 @@
 
 #define LED BIT6/* note that bit zero req'd for display */
 #define SWITCHES 15
-int gameOver = 0;
-int redrawScreen = 0;
+int gameOver;
+int redrawScreen;
 int switches;
 int birdX;
 int birdY;
 int birdWidth;
 int birdHeight;
 int gravity;
-int pause = 0;
+int pause;
 void jump();
 void reset();
 #endif //MAIN_H
