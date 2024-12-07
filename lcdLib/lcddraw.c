2e5ae995ec4a1fd34b769d8a3693120820f2e167
Alexis Marquez <alexismarq337@gmail.com>
2024-12-07 11:49:05 -0700

    Fixed another issue with draw char

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -84,9 +84,9 @@ void drawChar8x12(u_char rcol, u_char rrow, char c,
     u_char bit = 0x01;
     u_char oc = c - 0x20;
 
-    lcd_setArea(rcol, rrow, rcol + 4, rrow + 7); /* relative to requested col/row */
+    lcd_setArea(rcol, rrow, rcol + 7, rrow + 11); /* relative to requested col/row */
     while (row < 12) {
-        while (col < 12) {
+        while (col < 8) {
             u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
             lcd_writeColor(colorBGR);
             col++;
