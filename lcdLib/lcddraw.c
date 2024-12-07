0aaf0db59931efa006bad7ce71f6bbdc6d848c52
Alexis Marquez <alexismarq337@gmail.com>
2024-12-07 11:40:04 -0700

    Added draw char 8x12 and draw string functions

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -72,6 +72,41 @@ void drawChar5x7(u_char rcol, u_char rrow, char c,
   }
 }
 
+
+/** 8x12 font - this function draws background pixels
+ *  Adapted from RobG's EduKit
+ */
+void drawChar8x12(u_char rcol, u_char rrow, char c,
+     u_int fgColorBGR, u_int bgColorBGR)
+{
+    u_char col = 0;
+    u_char row = 0;
+    u_char bit = 0x01;
+    u_char oc = c - 0x20;
+
+    lcd_setArea(rcol, rrow, rcol + 4, rrow + 7); /* relative to requested col/row */
+    while (row < 12) {
+        while (col < 12) {
+            u_int colorBGR = (font_5x7[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+            lcd_writeColor(colorBGR);
+            col++;
+        }
+        col = 0;
+        bit <<= 1;
+        row++;
+    }
+}
+
+void drawString8x12(u_char col, u_char row, char *string,
+        u_int fgColorBGR, u_int bgColorBGR)
+{
+    u_char cols = col;
+    while (*string) {
+        drawChar8x12(cols, row, *string++, fgColorBGR, bgColorBGR);
+        cols += 11;
+    }
+}
+
 /** Draw string at col,row
  *  Type:
  *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
